2d88fffcdea2bc9e78c83c912f8277453766db35
lliqa <llcct@outlook.com>
2024-12-05 15:13:32 +0800

    Finished lab-cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,16 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct {
+  struct spinlock lock;
+  int cnt[PHYSTOP / PGSIZE];
+} ref;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +41,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    ref.cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,17 +59,21 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  acquire(&ref.lock);
+  if (--ref.cnt[(uint64)pa / PGSIZE] == 0) {
+    release(&ref.lock);
 
-  r = (struct run*)pa;
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    r = (struct run*)pa;
+
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  } else release(&ref.lock);
 }
-
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
@@ -72,11 +84,63 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    ref.cnt[(uint64)r / PGSIZE] = 1;
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+int iscowpage(pagetable_t pagetable, uint64 va) {
+    if (va >= MAXVA) return -1;
+    pte_t *pte = walk(pagetable, va, 0);
+    if (pte == 0) return -1;
+    if ((*pte & PTE_V) == 0) return -1;
+    if (*pte & PTE_C) return 0;
+    return -1;
+}
+
+void* cowalloc(pagetable_t pagetable, uint64 va) {
+    if (va % PGSIZE != 0) return 0;
+
+    uint64 pa = walkaddr(pagetable, va);
+    if (pa == 0) return 0;
+
+    pte_t *pte = walk(pagetable, va, 0);
+    
+    if (krefcnt((char *)pa) == 1) {
+        *pte |= PTE_W;
+        *pte ^= PTE_C;
+        return (void*)pa;
+    }
+    char *mem = kalloc();
+    if (mem == 0) return 0;
+
+    memmove(mem, (char *)pa, PGSIZE);
+    *pte ^= PTE_V;
+
+    if (mappages(pagetable, va, PGSIZE, (uint64)mem, ((PTE_FLAGS(*pte) | PTE_W) ^ PTE_C)) != 0) {
+        kfree(mem);
+        *pte |= PTE_V;
+        return 0;
+    }
+    kfree((char *)PGROUNDDOWN(pa));
+    return mem;
+}
+
+int krefcnt(void *pa) {
+    return ref.cnt[(uint64)pa / PGSIZE];
+}
+
+int kaddrefcnt(void *pa) {
+    if ((uint64)pa % PGSIZE != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+        return -1;
+    acquire(&ref.lock);
+    ++ref.cnt[(uint64)pa / PGSIZE];
+    release(&ref.lock);
+    return 0;
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -303,7 +303,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -312,13 +311,17 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    
+    if (flags & PTE_W) {
+        flags -= PTE_W;
+        flags |= PTE_C;
+        *pte = (PA2PTE(pa) | flags);
+    }
+
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       goto err;
     }
+    kaddrefcnt((char *)pa);
   }
   return 0;
 
@@ -351,6 +354,11 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
+
+    if (iscowpage(pagetable, va0) == 0) {
+        pa0 = (uint64)cowalloc(pagetable, va0);
+    }
+
     if(pa0 == 0)
       return -1;
     n = PGSIZE - (dstva - va0);

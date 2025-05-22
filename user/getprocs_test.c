b6150e27fae176636448fc15a99775bb0ee46f6a
lliqa <llcct@outlook.com>
2025-05-22 10:52:38 +0800

    lab01

--- user/getprocs_test.c
+++ user/getprocs_test.c
@@ -1,9 +1,9 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-    int cnt = getprocs();
-    printf("processes count: %d\n", cnt);
-    exit(0);
+int main(int argc, char *argv[])
+{
+  int count = getprocs();
+  printf("There are %d active processes.\n", count);
+  exit(0);
 }
